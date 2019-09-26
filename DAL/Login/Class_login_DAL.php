9f7f10ed055f52c8ed545abf864eb1af667481bf
Sidney Jr <44208657+sidjr12@users.noreply.github.com>
2019-09-25 21:05:18 -0300

    Adição
    Adição de uma variável de sessão.

--- DAL/Login/Class_login_DAL.php
+++ DAL/Login/Class_login_DAL.php
@@ -10,7 +10,7 @@
             $senha =  $objeto['senha'];        
             
             //cria a querry
-            $sql = "SELECT TBU.usernick, TBU.pessoa, TBP.nome, TBP.foto FROM TB_Usuario AS TBU, TB_Pessoa AS TBP WHERE TBU.email = '$email' && TBU.senha = '$senha' && TBU.pessoa = TBP.cod_pessoa LIMIT 1";
+            $sql = "SELECT TBP.tipo, TBU.usernick, TBU.pessoa, TBP.nome, TBP.foto FROM TB_Usuario AS TBU, TB_Pessoa AS TBP WHERE TBU.email = '$email' && TBU.senha = '$senha' && TBU.pessoa = TBP.cod_pessoa LIMIT 1";
             
             //chama função que vai conectar ao banco e executar a query
             $resultado = Func_executeselect_DAL($sql);//localizada no arquivo ../Class_conexão_DAL, linha 27
@@ -28,6 +28,7 @@
                 $objeto['usernick'] = $resultado['usernick'];
                 $objeto['nome'] = $resultado['nome'];
                 $objeto['foto'] = $resultado['foto'];
+                $_SESSION['Categoria'] = $resultado['tipo'];
                 $objeto['auxiliar'] = "logar";           
             }
         }
